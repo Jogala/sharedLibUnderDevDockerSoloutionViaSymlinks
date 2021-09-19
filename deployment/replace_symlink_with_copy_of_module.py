020d4b62afafd4f363d62d0bb9b8cf5656b0e6c3
joachim rosenberger <joachim.rosenberger@conovum.de>
2021-09-19 19:32:38 +0200

    remove moulde part removed from sciprt...

--- deployment/replace_symlink_with_copy_of_module.py
+++ deployment/replace_symlink_with_copy_of_module.py
@@ -70,28 +70,6 @@ for pathModul in pathsModuls:
 #%%
 with open('symlinksReplacedWithModules_paths.pkl', 'wb') as file:
     pickle.dump(srcDestPaths, file)
-
-#%%
-with open ('symlinksReplacedWithModules_paths.pkl', 'rb') as file:
-    srcDestPaths = pickle.load(file)
-
-#%%
-for [pathSrc,pathDest] in srcDestPaths:
-    
-    print('removing module ',pathDest)
-    shutil.rmtree(pathDest)
-    
-    if(pathSrc.split('../')[0] == ''):
-        print('relative path ',pathDest)
-        pathSrc = '../' + pathSrc
-    else:
-        print('absolute path ',pathSrc)
-        pass
-    
-    print('inserting symlink ',pathDest)
-    os.symlink(pathSrc,pathDest)
-    
-    
     
     
     
